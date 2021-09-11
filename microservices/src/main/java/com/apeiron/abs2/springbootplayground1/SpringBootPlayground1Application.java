9f6fe300cb1906b9b5a2d48d0ec7b463fcf36456
sb4aa4bs <bharath.softy@gmail.com>
2021-09-11 10:25:25 -0400

    Spring-Boot-Playground1: Initial Commit.

---
+++ microservices/src/main/java/com/apeiron/abs2/springbootplayground1/SpringBootPlayground1Application.java
@@ -0,0 +1,13 @@
+package com.apeiron.abs2.springbootplayground1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlayground1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlayground1Application.class, args);
+	}
+
+}
---
+++ microservices/src/test/java/com/apeiron/abs2/springbootplayground1/SpringBootPlayground1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apeiron.abs2.springbootplayground1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlayground1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
